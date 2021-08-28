d9988761f06ade57d9362b00ad2a5c2c01cbd967
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-28 23:03:35 +0530

    init commit

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+export default function ExpenseDate(props){
+
+    const day = props.date.toLocaleString('en-US',{day:'2-digit'})
+    const month = props.date.toLocaleString('en-US',{month:'long'})
+    const year = props.date.getFullYear();
+
+    return(
+
+        <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+        </div>
+
+    )
+}
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+
+export default function ExpenseItem(props){
+    const {title,date,amount} = props
+
+    
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={date}/>
+
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount}</div>
+            </div>
+        </div>
+    )
+}
