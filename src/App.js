6a7d41b95e581c6b38a98396e72de57ca3aedb9d
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-30 22:13:12 +0530

    Final staging

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Expenses from "./components/Expenses/Expenses";
+
 function App() {
+
+ 
+
+  
   
   return (
     <div>
-      <h2>Let's get started!</h2>
-
+      
      <Expenses/>
     </div>
    
