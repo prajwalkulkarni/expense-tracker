a2d9a338469202eee0f26a74f009ac9e081502fb
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-30 22:15:25 +0530

    Add expense chart & expense filter

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,31 @@
+import Chart from "../Chart/Chart"
+
+export default function ExpensesChart(props){
+
+    let chartData = [
+         {label:'Jan',value:0},
+         {label:'Feb',value:0},
+         {label:'Mar',value:0},
+         {label:'Apr',value:0},
+         {label:'May',value:0},
+         {label:'Jun',value:0},
+         {label:'Jul',value:0},
+         {label:'Aug',value:0},
+         {label:'Sep',value:0},
+         {label:'Oct',value:0},
+         {label:'Nov',value:0},
+         {label:'Dec',value:0},
+    ]
+
+    //alert(JSON.stringify(props.expenses))
+    for(let expenses=0;expenses<props.expenses.length;++expenses){
+        //alert(JSON.stringify(expenses))
+        let expenseMonth = props.expenses[expenses].date.getMonth()
+        chartData[expenseMonth].value += parseInt(props.expenses[expenses].amount)
+    }
+    //alert(JSON.stringify(chartData))
+
+
+    return <Chart dataPoints={chartData}/>
+
+}
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+  const arr = ['2011','2012','2013','2014','2015','2016','2017','2018','2019']
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={(e)=>props.yearHandler(e)} value={props.selected}>{
+          arr.map(obj=>
+           <option key={Math.random()*Math.random()}>{obj}</option>
+          )}
+          
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
