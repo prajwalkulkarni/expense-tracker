669c62476f1334056baa8438e8866d02c4364120
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-31 23:28:33 +0530

    Styled component for div and h1

---
+++ src/StyledComponent/Title.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+margin-top:0;
+padding:10px;
+`
+
+export default Title;
---
+++ src/StyledComponent/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    text-align: center;
+    display: block;
+    background-color: #002984;
+    color: #fff;
+    margin-top: 0;
+`;
+
+export default Wrapper
