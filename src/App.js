c7960de6a250afc7fb7406bfa415061cb082478d
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-08-31 23:33:24 +0530

    import styled components to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Expenses from "./components/Expenses/Expenses";
+import Title from "./StyledComponent/Title";
+import Wrapper from "./StyledComponent/Wrapper";
 
 function App() {
-
- 
-
-  
-  
   return (
     <div>
+      <Wrapper>
+        <Title>
+          <b>Expense tracker</b>
+        </Title>
+      </Wrapper>
       
      <Expenses/>
     </div>
