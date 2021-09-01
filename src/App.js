942af8e79a20ecd6e692927862ef629522638b5d
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-01 16:07:10 +0530

    Pass modal props and styling updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import Expenses from "./components/Expenses/Expenses";
 import Title from "./StyledComponent/Title";
 import Wrapper from "./StyledComponent/Wrapper";
+import Modal from "./components/Modal/Modal";
+import {useState} from 'react'
+import DisplayModal from "./StyledComponent/DisplayModal";
 
 function App() {
+
+  const [modal,showModal] = useState({
+    visible:false,
+    message:""
+  })
+
+  function dismissModal(){
+    showModal({visible:false,message:""})
+  }
+
+  const modalHolderHandler = (arg)=>{
+    //showModal(true)
+    showModal({...modal,visible:true,message:arg})
+
+  }
+  
   return (
     <div>
+     
+      
       <Wrapper>
         <Title>
           <b>Expense tracker</b>
         </Title>
       </Wrapper>
       
-     <Expenses/>
+     <Expenses modalHolder = {modalHolderHandler}/>
+     {modal.visible&&    
+     <DisplayModal>
+        <Modal dismiss = {dismissModal} message={modal.message}/>
+      </DisplayModal>}
     </div>
+    
    
 
   );
+  
 }
 
 export default App;
