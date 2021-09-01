552afe940f5c6e95ead3d15f8a440bc77a01e982
Prajwal Kulkarni <prajwalkulkarni76@gmail.com>
2021-09-01 16:05:49 +0530

    Modal wrapper

---
+++ src/StyledComponent/DisplayModal.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const DisplayModal = styled.div`
+
+z-index:100;
+position:absolute;
+top:0;
+width:100%;
+height: calc(100vh);
+display:flex;
+align-items:center;
+justify-content:center;
+backdrop-filter:blur(10px);
+
+`
+
+export default DisplayModal
